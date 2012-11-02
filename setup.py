54d11e87ecb4067a522eb72356060044bb38500e
Jonas Wagner <jwagner@raptus.com>
2012-11-02 09:39:55 +0100

    Moved to github

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '2.0b3'
 setup(name='Products.DimensionWidget',
       version=version,
       description="Provides an archetypes widget to store a dimension (width and height) in a StringField",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
       classifiers=[
@@ -16,7 +16,7 @@ setup(name='Products.DimensionWidget',
       keywords='',
       author='Raptus AG',
       author_email='dev@raptus.com',
-      url='https://svn.plone.org/svn/collective/Products.DimensionWidget',
+      url='https://github.com/Raptus/Products.DimensionWidget',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['Products'],
